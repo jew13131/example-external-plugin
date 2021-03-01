f41e0cb54695d61366eb53d50631fc0e5c9b7fba
jew13131 <79845471+jew13131@users.noreply.github.com>
2021-03-01 12:44:17 +0000

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
     checkstyle
 }
 
-project.extra["GithubUrl"] = "https://github.com/<INSERT NAME>/<INSERT REPOSITORY>"
+project.extra["GithubUrl"] = "https://github.com/jew13131<C4>/<example-external-plugin>"
 
 apply<BootstrapPlugin>()
 
